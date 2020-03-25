5c67019ef847e5d15c1cfa18abc5b1bd5bec1fa3
gilad <47846710+giladfr-rnd@users.noreply.github.com>
2020-03-25 13:06:34 +0200

    try to add setuptools scm versioning

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cvmonitor",
+    use_scm_version=True,
     packages=find_packages(),
     #package_dir={"": "cvmonitor"},
     install_requires=open("requirements.txt").readlines(),
