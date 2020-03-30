9d2175d0cd8c6eb19659d31eacf60dd25814a768
Your Name <you@example.com>
2020-03-30 22:59:43 +0300

    orginze code

--- cvmonitor/cv.py
+++ cvmonitor/cv.py
@@ -15,7 +15,7 @@ from prometheus_client import Summary
 import pytesseract
 from pylab import imshow, show
 from .qr import generate_pdf, find_qrcode, read_codes
-from image_align import get_oriented_image, align_by_qrcode
+from .image_align import get_oriented_image, align_by_qrcode
 np.set_printoptions(precision=3)
 
 class ComputerVision:
@@ -48,7 +48,7 @@ class ComputerVision:
                     application/json:
                         schema:
                             type: array
-                            items: 
+                            items:
                                 type: object
                                 properties:
                                     data:
@@ -63,8 +63,6 @@ class ComputerVision:
                                         type: number
             """
             image = np.asarray(imageio.imread(request.data))
-            codes = []
-            decodedObjects = pyzbar.decode(image)
             codes = read_codes(image)
             return json.dumps(codes), 200, {'content-type':'application/json'}
 
@@ -90,31 +88,40 @@ class ComputerVision:
                         format: binary
 
             """
-            imdata= io.BytesIO(request.data())
-                    
-            imdata.seek(0)
-            
-            image = np.asarray(imageio.imread(request.data))
-
-            if os.environ.get('CVMONITOR_SAVE_BEFORE_ALIGN')=='TRUE':
-                imageio.imwrite('original_image.jpg',image)
+            use_exif = os.environ.get('CVMONITOR_ORIENT_BY_EXIF','TRUE')=='TRUE'
+            use_qr = os.environ.get('CVMONITOR_ORIENT_BY_QR','FALSE')=='TRUE'
             qrprefix = str(os.environ.get('CVMONITOR_QR_PREFIX','cvmonitor'))
             qrsize = int(os.environ.get('CVMONITOR_QR_TARGET_SIZE',100))
             boundery = float(os.environ.get('CVMONITOR_QR_BOUNDERY_SIZE',50))
-            detected_qrcode = find_qrcode(image, qrprefix)
+            align_image_by_qr = os.environ.get('CVMONITOR_SKIP_ALIGN','TRUE')=='FALSE'
+            save_before_align = os.environ.get('CVMONITOR_SAVE_BEFORE_ALIGN')=='TRUE'
+            save_after_align = os.environ.get('CVMONITOR_SAVE_AFTER_ALIGN')=='TRUE'
+
+            imdata = io.BytesIO(request.data)
+            image, detected_qrcode, _ = get_oriented_image(imdata, use_exif=use_exif, use_qr=use_qr)
+
+            headers =  {'content-type':'image/jpeg'}
+            if save_before_align:
+                imdata.seek(0)
+                with open('original_image.jpg','wb') as f:
+                    f.write(imdata)
+
             if detected_qrcode is None:
-                abort(400, "Could not find the qr code to aling the image")
-            data = detected_qrcode.data.decode()
-            image = rotate_by_qr_code(image, detected_qrcode)
-            detected_qrcode = find_qrcode(image, qrprefix)
-            if not (os.environ.get('CVMONITOR_SKIP_ALIGN')=='TRUE'):
-                image, _ = align_by_qrcode(image, detected_qrcode, qrsize=qrsize, boundery = boundery)
-            if os.environ.get('CVMONITOR_SAVE_AFTER_ALIGN')=='TRUE':
+                if align_image_by_qr:
+                    abort(400, "Could not align the image by qr code, no such code detected")
+            else:
+                headers['X-MONITOR-ID']= detected_qrcode.data.decode()
+
+            if align_image_by_qr:
+                image, _ = align_by_qrcode(image, detected_qrcode, qrsize, boundery, qrprefix)
+
+            if save_after_align:
                 imageio.imwrite('aligned_image.jpg',image)
+
             b = io.BytesIO()
             imageio.imwrite(b, image, format='jpeg')
             b.seek(0)
-            return b.read(), 200, {'content-type':'image/jpeg','X-MONITOR-ID': data}
+            return b.read(), 200, headers
 
         @self.blueprint.route('/run_ocr', methods=['POST'])
         def run_ocr():
@@ -159,7 +166,7 @@ class ComputerVision:
                         items:
                             type: object
                             properties:
-                                segment_name: 
+                                segment_name:
                                     type: string
                                 value:
                                     type: string
@@ -169,16 +176,19 @@ class ComputerVision:
                 self.model_ocr = monitor_ocr.build_model()
             if not self.model_ocr:
                 abort(500,'NN Model not found, could not run ocr')
+            
             data = request.json
             assert 'image' in data
             image = np.asarray(imageio.imread(base64.decodebytes(data['image'].encode())))
             if not data.get('segments'):
                 return json.dumps([]), 200, {'content-type':'application/json'}
+
             segments = data['segments']
             if len(segments) == 0:
                 return json.dumps([]), 200, {'content-type':'application/json'}
             texts = self.model_ocr.ocr(segments, image, threshold)
             results = []
+
             for s,t in zip(segments,texts):
                 results.append({'segment_name': s['name'], 'value':t})
             return json.dumps(results), 200, {'content-type':'application/json'}
@@ -190,7 +200,7 @@ class ComputerVision:
             each qr.
             The data in the qr code will be cvmonitor-title-16_random_characters
             ---
-            description: get pdf of qr codes 
+            description: get pdf of qr codes
             get:
             parameters:
             - in: path
--- cvmonitor/test/test_parts.py
+++ cvmonitor/test/test_parts.py
@@ -3,61 +3,11 @@ import cv2
 import numpy as np
 import imageio
 import os
-import base64
 import ujson as json
 import pytest
 from pylab import imshow, show
-from .. import cv
-
-
-def test_ping(client):
-
-    res = client.get(url_for('ping'))
-    assert res.data == b'pong'
-
-
-def test_cv_ping(client):
-    res = client.get(url_for('cv.ping'))
-    assert res.data == b'pong cv'
-
-
-def test_codes(client):
-    image = open(os.path.dirname(__file__)+'/data/barcode.png', 'rb').read()
-    assert len(image) > 0
-    res = client.post(url_for('cv.detect_codes'), data=image, headers={'content-type': 'application/png'})
-    assert res.json[0]['data'] == 'Foramenifera'
-
-
-def test_align(client):
-    image = open(os.path.dirname(__file__)+'/data/qrcode.png', 'rb').read()
-    assert len(image) > 0
-    os.environ['CVMONITOR_QR_PREFIX'] = 'http'
-    res = client.post(url_for('cv.align_image'), data=image, headers={'content-type': 'application/png'})
-    res_image = np.asarray(imageio.imread(res.data))
-    assert res_image.shape[0] > 0
-
-
-def test_align_whole_image(client):
-    image = open(os.path.dirname(__file__)+'/data/barcode_monitor.jpg', 'rb').read()
-    assert len(image) > 0
-    os.environ['CVMONITOR_QR_PREFIX'] = 'http'
-    res = client.post(url_for('cv.align_image'), data=image, headers={'content-type': 'application/png'})
-    res_image = np.asarray(imageio.imread(res.data))
-    assert res_image.shape[0] > 0
-
-
-def test_ocr(client):
-    image = open(os.path.dirname(__file__)+'/data/11.jpg', 'rb').read()
-    bbox_list = np.load(open(os.path.dirname(__file__)+'/data/11_recs.npy', 'rb'))
-    image_buffer = base64.encodebytes(image).decode()
-    data = {
-        'image': image_buffer,
-        'segments': [{'left': int(s[0]), 'top':int(s[1]), 'right':int(s[2]), 'bottom':int(s[3]), 'name':str(i)} for i, s in enumerate(bbox_list)]
-    }
-    res = client.post(url_for('cv.run_ocr'), json=data)
-    assert res.json == [{'segment_name': '0', 'value': '52'}, {'segment_name': '1', 'value': '15'}, {
-        'segment_name': '2', 'value': '93'}, {'segment_name': '3', 'value': '115'}, {'segment_name': '4', 'value': '45'}]
-
+from .. import cv, image_align
+import pylab
 
 def test_pdf_generate():
     cv.generate_pdf('test.pdf', 'something',4,6)
@@ -130,3 +80,21 @@ def test_generated_qr():
     warpped, M = cv.align_by_qrcode(image, qrcode)
     assert 'dba7b418e0ef450c' in qrcode.data.decode()
     assert warpped.shape[0] > 0
+
+
+def test_orient_by_qr():
+    res = []
+    rwarpped = []
+    print()
+    for i in [1,2,3,4]:
+        im_file = open(os.path.dirname(__file__)+f'/data/bad{i}.jpg','rb')
+        image, _, _ = image_align.get_oriented_image(im_file, use_qr=True)
+        res.append(image)
+        # Detect QR again, so it will be eaactly the same
+        qrcode = cv.find_qrcode(image, '')
+        print(qrcode)
+        warpped, M = cv.align_by_qrcode(image, qrcode)
+        rwarpped.append(warpped)
+        # The original rotaion was lossy...
+        assert np.median(res[0]-res[-1])<2.0
+    
