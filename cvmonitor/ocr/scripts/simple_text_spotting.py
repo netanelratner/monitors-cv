aa9d756d52af3bb151ebc78cf3eb43a26b73b243
moshes7 <moshes777@gmail.com>
2020-04-02 01:52:59 +0300

    read annotation file

--- cvmonitor/ocr/scripts/simple_text_spotting.py
+++ cvmonitor/ocr/scripts/simple_text_spotting.py
@@ -1,11 +1,78 @@
 import cv2
+import numpy as np
 from cvmonitor.ocr.text_spotting import text_spotting
 
 
+def read_annotation_file(file_path):
+
+    with open(file_path, 'r') as f:
+        lines = f.read().splitlines()
+
+    ann = {}
+    for line in lines:
+
+        words = line.split(',')
+        words = [word.strip() for word in words if len(word.strip()) > 0]
+
+        n = 0
+        N = len(words)
+        while n < N: # iterate over all words in current line
+
+            if n == 0:
+
+                current_dict = {}
+                key = words[n]
+                ann[key] = current_dict
+                n += 1
+
+            else:
+
+                if key == 'monitor':
+                    # array of 4 coordinates (x,y)
+                    corners = [words[n + 1], words[n + 2], words[n + 3], words[n + 4],
+                               words[n + 5], words[n + 6], words[n + 7], words[n + 8]]
+                    corners = [int(float(corner)) for corner in corners] # cast to int
+                    current_dict['corners'] = np.array(corners).astype(np.int)
+                    n += 9
+
+                else:
+
+                    word = words[n]
+
+                    if word =='val':
+                        # scalar value (int or float)
+                        val = words[n + 1]
+
+                        if val.isnumeric():
+                            # val is a number - check if int or float
+                            if is_int(val): # val is int
+                                val = int(val)
+                            else: # val is float
+                                val = float(val)
+                        # else - val is str
+
+                        current_dict['val'] = np.array(val, dtype=np.float)
+                        n += 2
+
+                    elif word == 'Position': # dtype is int
+                        # array of [left, top, width, height] or maybe [top, left , width, height]
+                        current_dict['box'] = np.array([words[n+1], words[n+2], words[n+3], words[n+4]], dtype=np.int)
+                        n += 5
+
+    return ann
 
 
+def is_int(val):
+    try:
+        num = int(val)
+    except ValueError:
+        return False
+    return True
 
 if __name__ == '__main__':
+
+    ann_file = 'cvmonitor/test/data/BneiZIon4_1.txt'
+    ann = read_annotation_file(ann_file)
 
     img_path = 'cvmonitor/test/data/11.jpg'
     img_path = 'cvmonitor/test/data/BneiZIon4_1.tiff'
