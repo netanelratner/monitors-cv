babe2fb98b3cd7cb12699bfb58c6ba4e7c7d7251
moshes7 <moshes777@gmail.com>
2020-04-03 10:47:23 +0300

    add warp with selection of output size and margin add output examples

--- cvmonitor/ocr/scripts/simple_text_spotting.py
+++ cvmonitor/ocr/scripts/simple_text_spotting.py
@@ -185,8 +185,11 @@ if __name__ == '__main__':
     os.makedirs(output_dir, exist_ok=True)
     img_name = os.path.basename(img_path).split('.')[0]
 
-    predict_on_warped = False
+    predict_on_warped = True
     display = False
+
+    aligned_image_size = (1280, 768)  # (width, height)! and NOT (rows, cols)
+    align_margin_percent = 10  # [%] valid values are 0-40
 
     # model parameters
     visualize = True
@@ -216,13 +219,13 @@ if __name__ == '__main__':
 
             # align img and annotions by corners
             # img_warped, M = align_by_4_corners(img, corners, shape_out=(1280,768), margin_percent=0.)
-            img_warped, M = align_by_4_corners(img, corners)
+            img_warped, M = align_by_4_corners(img, corners, new_image_size=aligned_image_size, margin_percent=align_margin_percent)
 
             # note that bounding boxes need not be aligned, since they will be taken from the aligned image
 
-            cv2.imshow('image', img)
-            cv2.imshow('warped', img_warped)
-            cv2.waitKey(0)
+            # cv2.imshow('image', img)
+            # cv2.imshow('warped', img_warped)
+            # cv2.waitKey(0)
 
             # get expected boxes
             expected_boxes = process_annotation_dict(ann)
@@ -249,7 +252,7 @@ if __name__ == '__main__':
         texts, boxes, scores, frame = model.forward(img_warped, expected_boxes=expected_boxes)
 
         # save output
-        out_name = os.path.join(output_dir, img_name + '_warped.png')
+        out_name = os.path.join(output_dir, img_name + '_warped_{}_{}_margin_{}.png'.format(aligned_image_size[0], aligned_image_size[1], align_margin_percent))
         cv2.imwrite(out_name, frame)
 
         if display:
